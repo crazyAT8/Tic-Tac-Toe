95d744366b366be5dba8d63c2579ad2ff574bb39
crazyAT8 <88wwatson@gmail.com>
2025-03-20 23:54:23 -0500

    startd the vue.js version 3/20/25

---
+++ tic-tac-toe-vue/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <h1>Welcome to Tic Tac Toe!</h1>
+      <TicTacToe />
+    </div>
+  </template>
+  
+  <script>
+  import TicTacToe from '@/components/TicTacToe.vue';
+  
+  export default {
+    components: {
+      TicTacToe,
+    },
+  };
+  </script>
